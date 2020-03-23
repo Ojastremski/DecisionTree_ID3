9bec1a48d38275a9ad0f471341cea85213b1e7c8
Ojastremski <55191539+Ojastremski@users.noreply.github.com>
2020-03-23 17:36:43 +0100

    Add files via upload
    adding functionality responsible for generating a decision tree

---
+++ build_decision_tree.php
@@ -0,0 +1,85 @@
+<?php 
+
+require("index.php")
+
+$columns = read_data_coumn_by_column("test2.txt");
+
+$data = cut_decision_column($columns);
+echo search_max_gain__create_collection($columns,$data[0],$data[1])[1];
+
+//first iteration
+echo body($columns)
+			
+$collection = search_max_gain__create_collection($columns,$data[0],$data[1])[0];
+$counter = 0;
+//next iterations
+echo decision_tree($collection,$counter);
+
+function body ($columns) {	
+	$data = $this->cut_decision_column($columns);
+	
+	echo "<pre>";
+	$occurrence = $this->count_occurrence($data[0],$data[1]);
+	print_r($occurrence);
+	echo "</pre>";
+	
+	echo "<h3>Entropia rozkładu prawdopodobieństwa</h3>";
+	print_r($this->entropy($data[1]));
+	
+	echo "<h3>Informacja atrybutu</h3>";
+	echo "<pre>";
+	print_r($this->information_for_each_column($data[0],$occurrence));
+	echo "</pre>";
+	
+	echo "<h3>GAIN(X,T)</h3>";
+	echo "<pre>";
+	$gain = $this->gain($data[1],$data[0],$occurrence);
+	print_r($this->gain($data[1],$data[0],$occurrence));
+	echo "</pre>";
+	
+	echo "<h3>GAIN RATIOS</h3>";
+	echo "<pre>";
+	print_r($this->gainRatios($data[1],$data[0],$occurrence));
+	echo "</pre>";
+}
+
+function decision_tree($collection,&$counter) {
+	$temp = array();
+				
+	$keys = array_keys($collection);
+	for ($i = 0; $i < count($keys); $i++){					
+		$data = cut_decision_column($collection[$keys[$i]]);
+		$gain = gain($data[1],$data[0],count_occurrence($data[0],$data[1]));
+					
+		echo "<div class='card'>";
+		echo "<div class='card-header' id='heading".$counter."'>";
+		echo "<h5 class='mb-0'>";
+		echo "<button class='btn btn-link collapsed' type='button' data-toggle='collapse' data-target='#collapse".$counter."' aria-expanded='false' aria-controls='collapse".$counter."'>";
+		if (count(array_unique($gain)) !== 1){
+			$single_collection = search_max_gain__create_collection($collection[$keys[$i]],$data[0],$data[1])[0];
+			array_push($temp,$single_collection);
+			echo search_max_gain__create_collection($collection[$keys[$i]],$data[0],$data[1])[1]."&nbsp;|&nbsp;Unikalna wartość ".$keys[$i]."</h4>";
+		}
+		else {
+			echo "<h4>Brak podziału | GAIN = 0</h4>";
+		}
+		echo "</button>";
+		echo "</h5>";
+		echo "</div>";
+		echo "<div id='collapse".$counter."' class='collapse' aria-labelledby='heading".$counter."' data-parent='#accordionExample'>";
+		echo "<div class='card-body'>";
+		body($collection[$keys[$i]]);
+		echo "</div>";
+		echo "</div>";
+		echo "</div>";
+								
+		$counter++;
+	}
+	if (!empty($temp)){
+		$collection = $temp;
+		for ($j = 0; $j < count($collection); $j++) {
+			decision_tree($collection[$j],$counter);
+		}	
+	}
+}
+?>
